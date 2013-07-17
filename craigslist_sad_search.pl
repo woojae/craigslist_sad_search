97af4050fae9f58b16f535d4e287eb5eec24e344
woojae <woojae@ableholemust-lm.corp.yahoo.com>
2013-07-17 12:01:53 -0700

    craigslist

---
+++ craigslist_sad_search.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/env perl
+# A simple script to search for system administrator jobs.
+# Just change the searches and cities arrays.  
+
+use strict;
+my @cities = qw{austin sfbay losangeles newyork sandiego boston chicago seattle portland};
+my @searches = qw{bash perl ruby python php puppet chef kvm xen hyper-v vmware aws openstack rackspace}; 
+
+for my $city (@cities) {
+	print "$city  \n";
+	for my $keyword (@searches) { 
+		my $url = "http://$city.craigslist.org/search/sad?query=$keyword&zoomToPosting=&srchType=A";
+		my $html = qx{ curl --silent -L '$url' };
+		my @matches = $html =~ /found (\d+)/;
+		my $count = $matches[0];
+		print "$keyword: $count ";
+	}
+	print "\n";
+}
