f665797019eada75d0c5b853481be4b7d19826ab
Woojae Lee <woojae.lee@gmail.com>
2014-04-17 21:47:18 -0700

    Making changes adjust to craigslist

---
+++ add_to_log_file.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+date >> log.txt
+./craigslist_sad_search.pl >> log.txt
--- craigslist_sad_search.pl
+++ craigslist_sad_search.pl
@@ -1,17 +1,17 @@
 #!/usr/bin/env perl
 # A simple script to search for system administrator jobs.
-# Just change the searches and cities arrays.  
+# Just change the searches and cities arrays.
 
 use strict;
 my @cities = qw{austin sfbay losangeles newyork sandiego boston chicago seattle portland};
-my @searches = qw{bash perl ruby python php puppet chef kvm xen hyper-v vmware aws openstack rackspace}; 
+my @searches = qw{bash perl ruby python php puppet chef kvm xen hyper-v vmware aws openstack rackspace};
 
 for my $city (@cities) {
 	print "$city  \n";
-	for my $keyword (@searches) { 
+	for my $keyword (@searches) {
 		my $url = "http://$city.craigslist.org/search/sad?query=$keyword";
 		my $html = qx{ curl --silent -L '$url' };
-		my @matches = $html =~ /resultcount.*(\d+)/;
+		my @matches = $html =~ /1 - (\d+)/;
 		my $count = $matches[0];
 		print "$keyword: $count ";
 	}
